0406f5061fc05124941dc6aa45cf82051b02dc65
Joshua Fick <86298317+archy1234@users.noreply.github.com>
2024-11-14 22:22:23 +0200

    helloWorld.py
    added file "helloWorld.py".

---
+++ helloWorld.py
@@ -0,0 +1,58 @@
+class GitIsAwesomeProgram:
+
+    def __init__(self):
+
+        self.menu_options = {
+            '1': self.print_git_is_awesome,
+            '2': self.greet_user,
+            '3': self.quit_program,
+        }
+
+    def print_git_is_awesome(self):
+
+        print('Git is awesome')
+    
+    def greet_user(self):
+            
+            user_input = input("Enter your name: ")
+
+            if user_input.strip():
+                
+                print(f'Hello, {user_input.strip()}!')
+
+            else:
+
+                print('Hello there!')
+
+    def quit_program(self):
+
+        print('Goodbye!')
+        exit()
+    
+    def display_menu(self):
+
+        print("\nMenu:")
+        print("1. Print 'Git is awesome'")
+        print("2. Greet user")
+        print("3. Quit")
+
+    def run(self):
+
+        print('Welcome to the Git is Awesome Program!')
+
+        while True:
+
+            self.display_menu()
+
+            choice = input("Enter your choice (1-3): ")
+            action = self.menu_options.get(choice)
+
+            if action:
+                
+                action()
+
+
+if __name__ == "__main__":
+        
+        program = GitIsAwesomeProgram()
+        program.run()
